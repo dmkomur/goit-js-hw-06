319ef1d70cbc465b8d02fe4c5d33aeec5b5e9d41
dmkomur <125804681+dmkomur@users.noreply.github.com>
2023-04-15 11:09:27 +0300

    1 done

--- js/task-01.js
+++ js/task-01.js
@@ -1,9 +1,11 @@
 const commonListRef = document.querySelector('#categories');
-const commonListItemRef = commonListRef.querySelectorAll('.item');
+const commonListItemRef = [...commonListRef.children];
 
-console.log(`Number of categories: ${commonListRef.children.length}`);
+console.log(`Number of categories: ${commonListRef.children.length} \n`);
+console.log('\n');
 
 commonListItemRef.forEach(item => {
     console.log(`Category: ${item.firstElementChild.textContent}`);
-    console.log(`Elements: ${ item.lastElementChild.children.length}`);
+    console.log(`Elements: ${item.lastElementChild.children.length}`);
+    console.log('\n');
 });
